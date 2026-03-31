763ffea682fc9c3cd47f90562178aa332b0f0a61
jamirkhan6 <ajzomirkhan6@gmail.com>
2026-03-31 23:44:48 +0600

    create balance check controller

--- src/controllers/account.controller.js
+++ src/controllers/account.controller.js
@@ -15,5 +15,35 @@ async function createAccountController(req, res) {
 }
 
 
+async function getUserAccountController(req, res) {
+    const accounts = await accountModel.find({ user : req.user._id })
 
-module.exports = {createAccountController};
+    res.status(200).json({
+        accounts
+    })
+}
+
+
+async function getAccountBalanceController(req, res) {
+    const { accountId } = req.params
+
+    const account = await accountModel.findOne({ 
+        _id : accountId._id,
+        user : req.user._id
+    })
+
+    if(!account) {
+        return res.status(404).json({
+            message : "balance checker account not found"
+        })
+    }
+
+    const balance = await account.getBalance();
+
+    res.status(200).json({
+        accountId : account._id,
+        balance : balance
+    })
+}
+
+module.exports = {createAccountController, getUserAccountController, getAccountBalanceController};
--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -5,8 +5,14 @@ const accountController = require("../controllers/account.controller")
 
 const router = express.Router();
 
-
+//create new account
 router.post("/", authMiddleware.authMiddleware, accountController.createAccountController)
+
+//get all account of the logged-in user
+router.get("/", authMiddleware.authMiddleware, accountController.getUserAccountController)
+
+//account balance check
+router.get("/balance/:accountId", authMiddleware.authMiddleware, accountController.getAccountBalanceController)
 
 
 module.exports = router;
